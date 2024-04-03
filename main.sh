6110f86e2430249774f879de656c471b13513487
Fuuya1102668 <c1102668@planet.kanazawa-it.ac.jp>
2024-04-03 23:01:52 +0900

    first commit

---
+++ main.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/bash
+
+cd txt2img-images/ 
+folderPath=`ls -t | head -n 1`
+cd "$folderPath"
+echo $folderPath
+
+newestFile=`ls -t | head -n 1`
+lastNewestFile=""
+
+trap 'echo "Ctrl+C pressed, exiting..."; exit' INT
+
+while true; do
+    newestFile=`ls -t | head -n 1`
+    if [ "${newestFile}" != "${lastNewestFile}" ]; then
+        echo "open $newestFile"
+        eog "$newestFile" &
+        lastNewestFile=${newestFile}
+    fi
+    sleep 1
+done
